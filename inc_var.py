9f6ae0b6188425dd9400fe016b6c7665c783bac8
giuseppecurro <giuseppe.curro1@gmail.com>
2016-02-11 09:31:20 +0100

    inizio
    Signed-off-by: giuseppecurro <giuseppe.curro1@gmail.com>

---
+++ inc_var.py
@@ -0,0 +1,39 @@
+# Tre threads gestiscono ciascuno un contatore che si incrementa di volta in volta
+# con valori casuali.
+# Chi arrivera' per primo a 100?
+# per avere il pid: ps aux | grep inc_var.py 
+# per avere i threads attivi: ps -T <pid>
+# top -H
+import threading
+import random
+import time
+
+exitFlag = 0
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        print "Starting " + self.name
+        self.gareggia();
+        print "Exiting " + self.name
+    def gareggia(self):
+        while self.counter<=100:
+                inc_var=random.randrange(1, 7, 1)    
+        	self.counter = self.counter+inc_var
+                print self.name, self.counter, "     (",inc_var,")"
+                time.sleep(1)
+        print self.name+" Ha finito!!!"
+
+# Create new threads
+thread1 = myThread(1, "Thread-1", 0)
+thread2 = myThread(2, "Thread-2", 0)
+thread3 = myThread(3, "Thread-3", 0)
+# Start new Threads
+thread1.start()
+thread2.start()
+thread3.start()
+print "Exiting Main Thread"
